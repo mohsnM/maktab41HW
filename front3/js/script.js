123ef45a0bdb745ad8ae3edc869925b74b33116b
mohsnM <mohsn.mohsni278@gmail.com>
2020-11-03 14:57:43 +0330

    create app for show better whit js,css,html

--- front3/js/script.js
+++ front3/js/script.js
@@ -3,14 +3,6 @@ let list2 = [87, 34, 21, 43, 234];
 let list3 = [[2, 3], 5, [[[2, 9], 4], 4], 1, 0];
 let list4 = [32, 23, 12, 65, 65, 12, 54];
 
-const toArray = (str) => {
-  let newArr = [];
-  for (let i = 0; i < str.length; i += 2) {
-    newArr.push(arr[i]);
-  }
-  return newArr;
-}
-
 const deleteByIndex = (arr, index) => {
   arr.splice(index, 1);
   return arr;
@@ -29,7 +21,7 @@ const combineAndSort = (...arr) => {
 
 const flatArray = (arr) => {
   arr = arr.join();
-  return arr.match(/[0-9]*[^,]/g);;
+  return arr.match(/[0-9]*[^,]/g);
 };
 
 const findByItem = (arr, item) => {
@@ -50,47 +42,50 @@ const removeDuplicateItems = (arr) => {
   return arr.filter((a, b) => arr.indexOf(a) === b);
 };
 
-console.log(flatArray(list2));
-
 /* the code above is main code of project */
 
-let code, result, arg1, arg2;
+let code, result, arg1, arg2, arg3;
 code = document.getElementById("code");
 arg1 = document.getElementById("arg1");
 arg2 = document.getElementById("arg2");
+arg3 = document.getElementById("arg3");
 result = document.getElementById("result");
+
+const toArray = (str) => {
+  return str.match(/[0-9]*[^\][,\s]/g);
+};
 
 function callDeleteByIndex() {
   code.innerHTML = "deleteByIndex = " + deleteByIndex + ";";
-  result.innerHTML = "[" + deleteByIndex([3, 4, 2], 2) + "]";
+  result.innerHTML ="[" + deleteByIndex(toArray(arg1.value), +arg2.value) + "]";
 }
 
 function callCombineAndSort() {
   code.innerHTML = "combineAndSort = " + combineAndSort + ";";
-  result.innerHTML = combineAndSort();
+  result.innerHTML = "[" + combineAndSort(toArray(arg1.value)) + "]";
 }
 
 function callFlatArray() {
   code.innerHTML = "flatArray = " + flatArray + ";";
-  result.innerHTML = flatArray();
+  result.innerHTML = "[" + flatArray(toArray(arg1.value)) + "]";
 }
 
 function callFindByItem() {
   code.innerHTML = "findByItem = " + findByItem + ";";
-  result.innerHTML = findByItem();
+  result.innerHTML = findByItem(toArray(arg1.value), arg2.value);
 }
 
 function callReplaceByItem() {
   code.innerHTML = "replaceByItem = " + replaceByItem + ";";
-  result.innerHTML = replaceByItem();
+  result.innerHTML = "[" + replaceByItem(toArray(arg1.value), arg2.value, arg3.value) + "]";
 }
 
 function callExtractNumberFromSrting() {
   code.innerHTML = "extractNumberFromSrting = " + extractNumberFromSrting + ";";
-  result.innerHTML = extractNumberFromSrting();
+  result.innerHTML = extractNumberFromSrting(arg1.value);
 }
 
 function callRemoveDuplicateItems() {
   code.innerHTML = "removeDuplicateItems = " + removeDuplicateItems + ";";
-  result.innerHTML = removeDuplicateItems();
+  result.innerHTML = "[" + removeDuplicateItems(toArray(arg1.value)) + "]";
 }

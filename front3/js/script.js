d7ef6626ff7712556e97838cd587b00027846cf6
mohsnM <mohsn.mohsni278@gmail.com>
2020-11-03 13:41:20 +0330

    add style, make it readble in html document

---
+++ front3/js/script.js
@@ -0,0 +1,90 @@
+let list1 = [3, 5, 9, 21, 43];
+let list2 = [87, 34, 21, 43, 234];
+let list3 = [[2, 3], 5, [[[2, 9], 4], 4], 1, 0];
+let list4 = [32, 23, 12, 65, 65, 12, 54];
+
+const deleteByIndex = (arr, index) => {
+  arr.splice(index, 1);
+  return arr;
+};
+
+const combineAndSort = (...arr) => {
+  let combine = [];
+  for (let i = 0; i < arr.length; i++) {
+    combine = combine.concat(arr[i]);
+  }
+  combine.sort((a, b) => {
+    return a - b;
+  });
+  return combine;
+};
+
+const flatArray = (arr) => {
+  arr = arr.join();
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += 2) {
+    newArr.push(arr[i]);
+  }
+  return newArr;
+};
+
+const findByItem = (arr, item) => {
+  return `index: ${arr.indexOf(item)}, item: ${item}`;
+};
+
+const replaceByItem = (arr, currentItem, newItem) => {
+  let currentItemIndex = arr.indexOf(currentItem);
+  arr[currentItemIndex] = newItem;
+  return arr;
+};
+
+const extractNumberFromSrting = (str) => {
+  return str.match(/[0-9]/g);
+};
+
+const removeDuplicateItems = (arr) => {
+  return arr.filter((a, b) => arr.indexOf(a) === b);
+};
+
+/* the code above is main code of project */
+
+let code, result, arg1, arg2;
+code = document.getElementById("code");
+arg1 = document.getElementById("arg1");
+arg2 = document.getElementById("arg2");
+result = document.getElementById("result");
+
+function callDeleteByIndex() {
+  code.innerHTML = "deleteByIndex = " + deleteByIndex + ";";
+  result.innerHTML = "[" + deleteByIndex([3, 4, 2], 2) + "]";
+}
+
+function callCombineAndSort() {
+  code.innerHTML = "combineAndSort = " + combineAndSort + ";";
+  result.innerHTML = combineAndSort();
+}
+
+function callFlatArray() {
+  code.innerHTML = "flatArray = " + flatArray + ";";
+  result.innerHTML = flatArray();
+}
+
+function callFindByItem() {
+  code.innerHTML = "findByItem = " + findByItem + ";";
+  result.innerHTML = findByItem();
+}
+
+function callReplaceByItem() {
+  code.innerHTML = "replaceByItem = " + replaceByItem + ";";
+  result.innerHTML = replaceByItem();
+}
+
+function callExtractNumberFromSrting() {
+  code.innerHTML = "extractNumberFromSrting = " + extractNumberFromSrting + ";";
+  result.innerHTML = extractNumberFromSrting();
+}
+
+function callRemoveDuplicateItems() {
+  code.innerHTML = "removeDuplicateItems = " + removeDuplicateItems + ";";
+  result.innerHTML = removeDuplicateItems();
+}
